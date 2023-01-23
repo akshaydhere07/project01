73bce3a73b513ac87b10ca20d5b6492edaccb12a
akshay23052017 <akshay23052017@gmail.com>
2023-01-23 15:02:41 +0530

    Second Commit Folder added

---
+++ wp-content/themes/gym/functions.php
@@ -0,0 +1,31 @@
+<?php
+
+   // error_reporting(E_ALL);
+ini_set('display_errors', '1');
+/*This file is part of Leaverage, twentytwenty child theme.
+
+All functions of this file will be loaded before of parent theme functions.
+Learn more at https://codex.wordpress.org/Child_Themes.
+
+Note: this function loads the parent stylesheet before, then child theme stylesheet
+(leave it in place unless you know what you are doing.)
+*/
+
+if ( ! function_exists( 'suffice_child_enqueue_child_styles' ) ) {
+	function Gym_enqueue_child_styles() {
+	    // loading parent style
+	    wp_register_style(
+	      'parente2-style',
+	      get_template_directory_uri() . '/style.css'
+	    );
+
+	    wp_enqueue_style( 'parente2-style' );
+	    // loading child style
+	    wp_register_style(
+	      'childe2-style',
+	      get_stylesheet_directory_uri() . '/style.css'
+	    );
+	    wp_enqueue_style( 'childe2-style');
+	 }
+}
+add_action( 'wp_enqueue_scripts', 'Gym_enqueue_child_styles' );
